9bcedfd431cf6231534250050d3b71ac406dcb66
Mitraws <m.ws@outlook.com>
2020-04-30 02:01:00 +0200

    replaced <p> with <ul> for players

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -10,12 +10,22 @@ export default function Scoreboard(props) {
     { id: 4, name: "Lisa", score: 42 },
   ]);
   const player = players.map((players, index) => (
-    <Player key="players.id" name="players.name" />
+    <Player key={players.id} name={players.name} />
   ));
+
+//   const players_sorted =
+//   // first "copy" the array
+//   [...players]
+//   // then sort it with the `compare_score` callback function
+//   .sort(compare_score);
+
+// return // ...
+// }
 
   return (
     <div className="Scoreboard">
-      <h1>Scoreboard</h1>;<p>{player}</p>
+      <h1>Scoreboard</h1>
+      <ul>{player}</ul>
     </div>
   );
 }
